643c13826018954d8bae26f703ea84991affb532
Donald Wu <yeukfei02@gmail.com>
2021-11-08 17:51:25 +0800

    fix: 🐛 fix set_default_image func

--- app/models/group.rb
+++ app/models/group.rb
@@ -36,7 +36,7 @@ class Group < ApplicationRecord
   private
 
   def set_default_image
-    unless image.attached? && Rails.env != 'production'
+    unless image.attached? && Rails.env == 'test'
       image.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'group.jpg')),
                    filename: 'group.jpg', content_type: 'image/jpg')
     end
