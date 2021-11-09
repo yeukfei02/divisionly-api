0ead0aea2f8f50ea4f332750b0df889ebd566100
Donald Wu <yeukfei02@gmail.com>
2021-11-09 10:58:37 +0800

    fix: 🐛 fix database cleanup logic

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -73,9 +73,21 @@ RSpec.configure do |config|
     DatabaseCleaner.clean_with(:truncation)
   end
 
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
+  config.after(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+
+    s3 = Aws::S3::Client.new(access_key_id: ENV['USER_ACCESS_KEY_ID'],
+                             secret_access_key: ENV['USER_SECRET_ACCESS_KEY'], region: 'ap-southeast-1')
+    resp = s3.list_objects(bucket: 'divisionly-api-test')
+    if resp.present?
+      resp.contents.each do |object|
+        delete_resp = s3.delete_object({
+                                         bucket: 'divisionly-api-test',
+                                         key: object.key
+                                       })
+        puts "delete_resp = #{delete_resp}"
+      end
     end
   end
 end
