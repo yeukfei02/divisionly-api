b9c13a2983886f78f48d5013ac3ad0fa4c6174d7
Donald Wu <yeukfei02@gmail.com>
2021-11-09 09:59:14 +0800

    fix: 🐛 fix active storage table

--- db/migrate/20211104060717_create_active_storage_tables.active_storage.rb
+++ db/migrate/20211104060717_create_active_storage_tables.active_storage.rb
@@ -1,7 +1,7 @@
 # This migration comes from active_storage (originally 20170806125915)
 class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
   def change
-    create_table :active_storage_blobs do |t|
+    create_table :active_storage_blobs, id: primary_key_type do |t|
       t.string   :key,          null: false
       t.string   :filename,     null: false
       t.string   :content_type
@@ -14,24 +14,31 @@ class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
       t.index [:key], unique: true
     end
 
-    create_table :active_storage_attachments do |t|
+    create_table :active_storage_attachments, id: primary_key_type do |t|
       t.string     :name,     null: false
-      t.references :record,   null: false, polymorphic: true, index: false
-      t.references :blob,     null: false
+      t.references :record,   null: false, polymorphic: true, index: false, type: primary_key_type
+      t.references :blob,     null: false, type: primary_key_type
 
       t.datetime :created_at, null: false
 
       t.index %i[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness',
                                                       unique: true
-      t.foreign_key :active_storage_blobs, column: :blob_id
+      t.foreign_key :active_storage_blobs, column: :blob_id, type: primary_key_type
     end
 
-    create_table :active_storage_variant_records do |t|
-      t.belongs_to :blob, null: false, index: false
+    create_table :active_storage_variant_records, id: primary_key_type do |t|
+      t.belongs_to :blob, null: false, index: false, type: primary_key_type
       t.string :variation_digest, null: false
 
       t.index %i[blob_id variation_digest], name: 'index_active_storage_variant_records_uniqueness', unique: true
-      t.foreign_key :active_storage_blobs, column: :blob_id
+      t.foreign_key :active_storage_blobs, column: :blob_id, type: primary_key_type
     end
+  end
+
+  private
+
+  def primary_key_type
+    config = Rails.application.config.generators
+    config.options[config.orm][:primary_key_type] || :bigint
   end
 end
