9f5addaa5174be799e77e80f2733450094e369d9
Donald Wu <yeukfei02@gmail.com>
2021-11-09 15:31:01 +0800

    fix: 🐛 fix phone number in test case

--- spec/factories/friend.rb
+++ spec/factories/friend.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     association :user
     name { Faker::Lorem.word }
     description { Faker::Lorem.sentence }
-    phone_number { Faker::Number.number(digits: 8) }
+    phone_number { Faker::Number.number(digits: 8).to_s }
     user_id { user.id }
   end
 end
