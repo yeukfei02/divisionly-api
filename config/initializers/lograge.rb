09fd1966707ee94cc0fd4162e60b8a579072eef7
Donald Wu <yeukfei02@gmail.com>
2021-12-02 14:41:43 +0800

    fix: 🐛 add timezone

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -2,6 +2,6 @@ Rails.application.configure do
   config.lograge.enabled = true
 
   config.lograge.custom_options = lambda do |event|
-    { time: Time.now }
+    { time: Time.zone.now }
   end
 end
