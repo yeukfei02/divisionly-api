eefb8d4d4b288fb18aeb8a49d9eac96bed331efa
Donald Wu <yeukfei02@gmail.com>
2021-12-02 14:56:13 +0800

    fix: 🐛 fix rubocop

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.lograge.enabled = true
 
-  config.lograge.custom_options = lambda do |event|
+  config.lograge.custom_options = lambda do |_event|
     { time: Time.zone.now }
   end
 end
